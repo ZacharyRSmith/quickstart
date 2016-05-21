b87b0dcf3e51fd431016b7b39ce83936dca3f55c
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-21 09:28:49 -0700

    refactor: Move Hero details into own component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
+    directives: [HeroDetailComponent],
+
     selector: 'my-app',
 
     styles: [`
@@ -70,14 +70,7 @@ export class Hero {
         </li>
       </ul>
 
-      <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-          <label>name: </label>
-          <input [(ngModel)]="selectedHero.name" placeholder="name">
-        </div>
-      </div>
+      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `
 })
 export class AppComponent {
