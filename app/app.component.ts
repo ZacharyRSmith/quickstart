a353ae781e44d361981c2e0296c4bdadb5ed1f18
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-21 09:47:04 -0700

    refactor: Move data into a service.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
     directives: [HeroDetailComponent],
+
+    providers: [HeroService],
 
     selector: 'my-app',
 
@@ -73,23 +76,24 @@ import { HeroDetailComponent } from './hero-detail.component';
       <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `
 })
-export class AppComponent {
-  public heroes = HEROES;
+export class AppComponent implements OnInit {
+  constructor(private heroService: HeroService) {
+
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  heroes: Hero[];
   title = 'Tour of Heroes';
   selectedHero: Hero;
 
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => {
+      this.heroes = heroes;
+    });
+  }
+
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
