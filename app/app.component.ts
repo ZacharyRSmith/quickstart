35f1160537033ca3fe4a3eae5c492353cd290ace
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-05-21 20:48:39 -0700

    enhance: Make it stylin', yo! ; )

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,52 @@
 import { Component } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS }
+  from '@angular/router-deprecated';
 
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 
 @Component({
-  directives: [HeroesComponent],
+  directives: [ROUTER_DIRECTIVES],
 
-  providers: [HeroService],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+   ],
 
   selector: 'my-app',
 
+  styleUrls: ['app/app.component.css'],
+
   template: `
     <h1>{{title}}</h1>
-
-    <my-heroes></my-heroes>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `
 })
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 
 export class AppComponent {
   title = 'Tour of Heroes';
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,6 +4,11 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  getHero(id: number) {
+    return Promise.resolve(HEROES)
+      .then(heroes => heroes.find(hero => hero.id === id) || [])
+  }
+
   getHeroes() {
     return Promise.resolve(HEROES);
   }
